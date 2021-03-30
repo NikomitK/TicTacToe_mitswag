b51a59a4e175111fda6a01c275131914cf5af6af
NikomitK <nikobo2003@gmail.com>
2021-03-30 11:17:15 +0200

    Initial commit

---
+++ src/de/NikomitK/TicTacToe/Gui.java
@@ -0,0 +1,5 @@
+package de.NikomitK.TicTacToe;
+
+public class Gui {
+    
+}
---
+++ src/de/NikomitK/TicTacToe/Main.java
@@ -0,0 +1,8 @@
+package de.NikomitK.TicTacToe;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    
+    }
+}
