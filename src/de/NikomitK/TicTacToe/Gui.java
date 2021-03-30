bf04ab6856c813d4070b5f8b40065935e463a1ca
NikomitK <nikobo2003@gmail.com>
2021-03-30 11:15:47 +0200

    some more lines but nothing serious lmao

--- src/de/NikomitK/TicTacToe/Gui.java
+++ src/de/NikomitK/TicTacToe/Gui.java
@@ -1,15 +1,21 @@
 package de.NikomitK.TicTacToe;
 
-public class Gui {
+import javax.swing.*;
 
+public class Gui extends JFrame {
+
+    public static Ticfeld tf;
+    private static JPanel knopfFeld;
 
 
     private static void erzeugeFeld(){
+        tf  = new Ticfeld();
+
 
     }
 
     public static void main(String[] args){
-
+        erzeugeFeld();
     }
 
 }
--- src/de/NikomitK/TicTacToe/Ticfeld.java
+++ src/de/NikomitK/TicTacToe/Ticfeld.java
@@ -1,10 +1,19 @@
 package de.NikomitK.TicTacToe;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
 
 public class Ticfeld {
     JButton [] buttons = new JButton[9];
 
+    public Ticfeld(){
+        for(int i = 0; i < 9; i++){
+            buttons[i] = new JButton();
+            buttons[i].addActionListener((ActionEvent e) -> {
+                if(buttons[i].getText() != null) buttons[i].setText();
+            });
+        }
+    }
     
 
 }
